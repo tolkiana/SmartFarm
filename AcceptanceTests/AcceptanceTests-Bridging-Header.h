ed9e66cb42f0ec0b28009e4fa81541380a87ba8a
Nelida Velazquez <tolkiana@gmail.com>
2017-03-28 21:01:45 -0400

    Creates AcceptanceTests target

---
+++ AcceptanceTests/AcceptanceTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  AcceptanceTests-Bridging-Header.h
+//  OCSlimProject
+//
+//  Created by OCSlimProject on 3/28/17.
+//  MIT License
+//  © 2016 Paul Stringer
+//
+
+
+
+#import "SlimTables.h"
