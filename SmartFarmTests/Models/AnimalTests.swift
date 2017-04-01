f50a1a7747b5a019d2f64833d367e74cd6affdcf
Nelida Velazquez <tolkiana@gmail.com>
2017-04-01 18:25:25 -0400

    Updates test description

--- SmartFarmTests/Models/AnimalTests.swift
+++ SmartFarmTests/Models/AnimalTests.swift
@@ -13,6 +13,6 @@ class AnimalTests: XCTestCase {
     
     func testAnimalDescription() {
         let cow = Animal(category: .cow, price: 100.0, weight: 100.0, vaccinated: true)
-        XCTAssert(cow.description == AnimalCategory.cow.rawValue, "The description should be the category name")
+        XCTAssert(cow.description == AnimalCategory.cow.rawValue, "The store Item description should be the category name")
     }
 }
