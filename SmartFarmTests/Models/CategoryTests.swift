2c98f94a02455e5a83db8a2d933a28f0da7ea6e7
Nelida Velazquez <tolkiana@gmail.com>
2017-04-02 22:23:38 -0400

    Create test classes and create methods, will implement later

---
+++ SmartFarmTests/Models/CategoryTests.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryTests.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/2/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import XCTest
+@testable import SmartFarm
+
+class CategoryTests: XCTestCase {
+    
+    func test_successfull_initialization() {
+        let category = Category(
+    }
+    
+    func test_failed_initialization_due_missing_key() {
+        
+    }
+    
+    func test_failed_initialization_due_invalid_key() {
+        
+    }
+        
+}
---
+++ SmartFarmTests/Models/StoreItemTests.swift
@@ -0,0 +1,28 @@
+//
+//  StoreItemTests.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/2/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import XCTest
+@testable import SmartFarm
+
+class StoreItemTests: XCTestCase {
+    
+    func test_successfull_initialization() {
+        
+    }
+    
+    func test_failed_initialization_due_missing_key() {
+        
+    }
+    
+    func test_failed_initialization_due_invalid_key() {
+        
+    }
+
+
+    
+}
