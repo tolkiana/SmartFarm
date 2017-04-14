808bd062dfeb6f3f0aa21c56046f896eb9e7ae99
tolkiana <tolkiana@gmail.com>
2017-04-14 14:22:07 -0400

    Creates empty NetworkManager class

---
+++ SmartFarm/Network/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/14/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
