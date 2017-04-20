d63f7650c1327eac7e4f0356fc7779296882f1c7
tolkiana <tolkiana@gmail.com>
2017-04-20 17:13:28 -0400

    Adding a ResultType enum to pass as parameter in callbacks for closures

---
+++ SmartFarm/Network/ResultType.swift
@@ -0,0 +1,14 @@
+//
+//  ResultType.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/20/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType<T> {
+    case Success(T)
+    case Failure(Error)
+}
