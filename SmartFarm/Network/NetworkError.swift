9565471836ce02f208df19c5aab5239cb35c471e
tolkiana <tolkiana@gmail.com>
2017-04-20 17:47:27 -0400

    Add an enum for Network errors, will add more cases later

---
+++ SmartFarm/Network/NetworkError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkError.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/20/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case notFound
+    case noData
+}
