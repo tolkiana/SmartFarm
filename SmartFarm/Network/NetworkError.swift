f040a355f8c2786fe3e94e8ec08980b972d925a3
tolkiana <tolkiana@gmail.com>
2017-04-22 21:51:08 -0400

    Adding more types of errors and an initializer

--- SmartFarm/Network/NetworkError.swift
+++ SmartFarm/Network/NetworkError.swift
@@ -11,4 +11,22 @@ import Foundation
 enum NetworkError: Error {
     case notFound
     case noData
+    case badRequest
+    case forbidden
+    case parse
+    case noResponse
+    case unknown
+    
+    init(code: Int) {
+        switch code {
+        case 400:
+            self = .badRequest
+        case 401:
+            self = .notFound
+        case 400:
+            self = .forbidden
+        default:
+            self = .unknown
+        }
+    }
 }
