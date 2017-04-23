43039a003295fbc05461fb128f59968f373d3a34
tolkiana <tolkiana@gmail.com>
2017-04-22 22:16:05 -0400

    Defining tests for NetworkManager

---
+++ SmartFarmTests/Network/NetworkManagerTests.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkManagerTests.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/22/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import XCTest
+
+class NetworkManagerTests: XCTestCase {
+    
+    func test_URLSession_error() {
+    
+    }
+    
+    func test_no_response_error() {
+    
+    }
+    
+    func test_not_found_error() {
+        
+    }
+    
+    func test_forbidden_error() {
+        
+    }
+    
+    func test_unknown_error() {
+        
+    }
+    
+    func test_no_data_error() {
+        
+    }
+    
+    func test_serialization_error() {
+        
+    }
+    
+    func test_successfull_execution() {
+    
+    }
+    
+}
