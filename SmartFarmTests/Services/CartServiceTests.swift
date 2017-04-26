97e5def167d4089adf79ebccbc70bc03f4dd93cc
tolkiana <tolkiana@gmail.com>
2017-04-25 22:37:32 -0400

    Creates empty test files, will implement in the next commit

---
+++ SmartFarmTests/Services/CartServiceTests.swift
@@ -0,0 +1,13 @@
+//
+//  CartServiceTests.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/22/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import XCTest
+
+class CartServiceTests: XCTestCase {
+    
+}
---
+++ SmartFarmTests/Services/CatalogServiceTests.swift
@@ -0,0 +1,14 @@
+//
+//  CatalogServiceTests.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 4/22/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import XCTest
+
+class CatalogServiceTests: XCTestCase {
+    
+    
+}
