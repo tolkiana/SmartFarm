086de525d235af7dc1e385f9f739bac8813e308b
tolkiana <tolkiana@gmail.com>
2017-04-30 14:21:45 -0400

    Adds empty definitions for all cart tests

--- SmartFarmTests/Services/CartServiceTests.swift
+++ SmartFarmTests/Services/CartServiceTests.swift
@@ -10,4 +10,48 @@ import XCTest
 
 class CartServiceTests: XCTestCase {
     
+    override func tearDown() {
+        CartService.shared.clearItems()
+    }
+    
+    func test_getting_all_cart_items() {
+    
+    }
+    
+    func test_getting_total_items() {
+    
+    }
+    
+    func test_add_new_store_item() {
+    
+    }
+    
+    func test_add_existing_store_item() {
+        
+    }
+    
+    func test_remove_cart_item() {
+    
+    }
+    
+    func test_increment_cart_item() {
+    
+    }
+    
+    func test_decrement_cart_item() {
+    
+    }
+    
+    func test_decrement_last_cart_item() {
+    
+    }
+    
+    func test_clear_items() {
+    
+    }
+    
+    func test_checkout() {
+    
+    }
+    
 }
