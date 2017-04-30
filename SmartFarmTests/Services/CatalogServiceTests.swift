5ee4d9d13262c523ec4b6f0bfc155786ddf0270f
tolkiana <tolkiana@gmail.com>
2017-04-30 00:05:56 -0400

    Adds tests for CatalogService

--- SmartFarmTests/Services/CatalogServiceTests.swift
+++ SmartFarmTests/Services/CatalogServiceTests.swift
@@ -11,34 +11,50 @@ import XCTest
 
 class CatalogServiceTests: XCTestCase {
     
-    
-    
+    let mockService = MockCatalogService()
     
     func testAllCategories() {
-    
+        CatalogService.shared.loadData {}
+        let categories = CatalogService.shared.allCategories()
+        
+        XCTAssertNotNil(categories)
     }
     
     func testAllItems() {
+        let items = mockService.allCategories()?.map{ $0.items }
+        let expectedItems = mockService.allItems()
         
+        XCTAssert(expectedItems?.count == items?.count)
     }
     
     func testCategoryWithCode() {
-    
+        let rawCategory = JSONReader.array(fromJSONfile: "catalog").first!
+        let items = rawCategory["items"] as! [[String: Any]]
+        let category = mockService.category(with: "001")
+        
+        XCTAssert(category?.code == rawCategory["code"] as? String)
+        XCTAssert(category?.name == rawCategory["name"] as? String)
+        XCTAssert(category?.items.count == items.count)
     }
     
-    func testItemsForCategory() {
-    
-    }
     
     func testItemWithCode() {
-    
+        let animals = JSONReader.array(fromJSONfile: "catalog").first!["items"] as! [[String: Any]]
+        let cow =  animals.first!
+        let item = mockService.item(with: "001")
+        
+        XCTAssert(item?.code == cow["code"] as? String)
+        XCTAssert(item?.name == cow["name"] as? String)
+        XCTAssert(item?.icon == cow["image"] as? String)
+        XCTAssert(item?.price == cow["price"] as? Float)
+        XCTAssert(item?.numberAvailable == cow["numberAvailable"] as? Int)
     }
     
-    func totalItemsForCategory() {
-    
-    }
-    
-    func totalItems() {
-    
+    func testTotalItems() {
+        let animals = JSONReader.array(fromJSONfile: "catalog").first!["items"] as! [[String: Any]]
+        let vegetables = JSONReader.array(fromJSONfile: "catalog").last!["items"] as! [[String: Any]]
+        let total = mockService.totalItems()
+        
+        XCTAssert(total == animals.count + vegetables.count)
     }
 }
