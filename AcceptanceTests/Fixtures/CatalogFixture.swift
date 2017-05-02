51b951960d2b691aaafd7888e3d8bca4082da457
tolkiana <tolkiana@gmail.com>
2017-05-02 19:37:54 -0400

    Adds CatalogFixture

---
+++ AcceptanceTests/Fixtures/CatalogFixture.swift
@@ -0,0 +1,23 @@
+//
+//  CatalogFixture.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 5/2/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+@objc(CatalogFixture)
+class CatalogFixture: NSObject {
+    var animal = ""
+    
+    func price() -> String {
+        return ""
+    }
+    
+    func quantity() -> String {
+        return ""
+    }
+    
+}
