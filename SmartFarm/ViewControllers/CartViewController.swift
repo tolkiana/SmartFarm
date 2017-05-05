7f83dd955c58a3b0e5cac7ded838c0a01df3399a
tolkiana <tolkiana@gmail.com>
2017-05-04 22:24:18 -0400

    Creates Catalog and Cart ViewControllers

---
+++ SmartFarm/ViewControllers/CartViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CartViewController.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 5/4/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var checkoutButton: UIBarButtonItem!
+}
---
+++ SmartFarm/ViewControllers/CatalogViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CatalogViewController.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 3/28/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class CatalogViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var cartButton: UIBarButtonItem!
+}
+
