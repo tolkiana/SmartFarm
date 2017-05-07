0d509f7ea18570485dce69747bb2598097e8eaa3
tolkiana <tolkiana@gmail.com>
2017-05-07 17:01:21 -0400

    Creates a file for constants, for now adds the cell identifiers

---
+++ SmartFarm/Constants/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 5/7/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+struct StoryboardConstants {
+    struct CellIdentifiers {
+        static let catalogItemCell = "CatalogItemCellIdentifier"
+        static let cartItemCell = "CartItemCellIdentifier"
+        static let totalItemCell = "TotalCellIdentifier"
+    }
+}
