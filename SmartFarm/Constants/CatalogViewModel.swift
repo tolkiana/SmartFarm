710d20b665cc0c15b3a40a7a2864e15875a34d6d
tolkiana <tolkiana@gmail.com>
2017-05-07 18:53:47 -0400

    Creates empty structs for CatalogViewModel and CartViewModel

---
+++ SmartFarm/Constants/CatalogViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CatalogViewModel.swift
+//  SmartFarm
+//
+//  Created by Nelida Velazquez on 5/7/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+struct CatalogViewModel {
+    
+}
